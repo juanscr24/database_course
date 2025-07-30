855079e82dde6b0ff2c7815db8a450553bc40156
Juan <juanscr24@hotmail.com>
2025-07-30 11:09:56 -0500

    first commit

---
+++ Statements/01_select.sql
@@ -0,0 +1,6 @@
+-- Selecionar o traer algo de la tabla
+SELECT * FROM users;
+
+SELECT name FROM users;
+
+SELECT user_id, name FROM users;
---
+++ Statements/02_distinc.sql
@@ -0,0 +1,4 @@
+-- Trae valores de campos distintos, si se repiten, solo trae uno
+SELECT DISTINCT * FROM users;
+
+SELECT DISTINCT age FROM users;
